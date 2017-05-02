b6e9de125a1e105857a7d56b44ac2ec186dc6a8a
Alfonsus Raditya Arsadjaja <raditya1710@gmail.com>
2017-05-02 22:57:49 +0700

    Added drawElements array

--- texture_temp/coba.py
+++ texture_temp/coba.py
@@ -1,7 +1,7 @@
 '''
 This tutorial builds on earlier tutorials by adding:
-    * 	ambient lighting 
-    * 	diffuse lighting 
+    * 	ambient lighting
+    * 	diffuse lighting
     * 	directional lights (e.g. the Sun)
     * 	normals, the normal matrix
 '''
@@ -16,16 +16,16 @@ from OpenGL.GL.shaders import compileProgram, compileShader
 from texture import Texture
 
 DLIGHT_FUNC = """
-float dLight( 
+float dLight(
     in vec3 light_pos, // normalised light position
     in vec3 frag_normal // normalised geometry normal
 ) {
     // returns vec2( ambientMult, diffuseMult )
-    float n_dot_pos = max( 0.0, dot( 
+    float n_dot_pos = max( 0.0, dot(
         frag_normal, light_pos
     ));
     return n_dot_pos;
-}		
+}
 """
 
 VERTEX_SHADER = DLIGHT_FUNC + '''
@@ -45,7 +45,7 @@ varying vec2 texCoord0;
 
 void main() {
 
-    gl_Position = gl_ModelViewProjectionMatrix * vec4( 
+    gl_Position = gl_ModelViewProjectionMatrix * vec4(
         Vertex_position, 1.0
     );
 
@@ -57,16 +57,16 @@ void main() {
 
     texCoord0 = Vertex_texCoord;
 
-    baseColor = clamp( 
+    baseColor = clamp(
     (
-        // global component 
+        // global component
         (Global_ambient * Material_ambient)
 
-        // material's interaction with light's contribution 
+        // material's interaction with light's contribution
         // to the ambient lighting...
         + (Light_ambient * Material_ambient)
-        
-        // material's interaction with the direct light from 
+
+        // material's interaction with the direct light from
         // the light.
         + (Light_diffuse * Material_diffuse * diffuse_weight)
     ), 0.0, 1.0);
@@ -134,7 +134,10 @@ class TestContext( BaseContext ):
             array( VERTEX_DATA, 'f' )
         )
 
-        #self.indices = vbo.VBO(VERTEX_INDICES, target='GL_ELEMENT_ARRAY_BUFFER')
+        self.indices = vbo.VBO(
+            array(VERTEX_INDICES, 'uint32'),
+            target='GL_ELEMENT_ARRAY_BUFFER'
+        )
 
         self.tex = [
             Texture('res/depan.jpg'),
@@ -165,7 +168,7 @@ class TestContext( BaseContext ):
             if location in ( None, -1 ):
                 print 'Warning, no attribute: %s'%( attribute )
             setattr( self, attribute + '_loc', location )
-        
+
     def Render( self, mode ):
         '''
         render the scene geometry
@@ -173,51 +176,47 @@ class TestContext( BaseContext ):
         gl.glUseProgram( self.shader )
         try:
             self.vertices.bind()
-            #self.indices.bind()
+            self.indices.bind()
             self.tex[0].Bind(0)
-                
+
             try:
                 gl.glUniform4f( self.Global_ambient_loc, .9,.05,.05,.1 )
                 gl.glUniform4f( self.Light_ambient_loc, .2,.2,.2, 1.0 )
                 gl.glUniform4f( self.Light_diffuse_loc, 1,1,1,1 )
-                
+
                 gl.glUniform3f( self.Light_location_loc, 2,2,10 )
                 gl.glUniform4f( self.Material_ambient_loc, .2,.2,.2, 1.0 )
                 gl.glUniform4f( self.Material_diffuse_loc, 1,1,1, 1 )
-                
+
                 stride = 8*4 # x y z x_tex y_tex r g b * sizeof(float)
                 gl.glEnableVertexAttribArray( self.Vertex_position_loc ) # 0
                 gl.glEnableVertexAttribArray( self.Vertex_texCoord_loc ) # 1
                 gl.glEnableVertexAttribArray( self.Vertex_normal_loc ) # 2
 
-                print "Bind 0"
                 # bind the vertex attribute location
-                gl.glVertexAttribPointer( 
-                    self.Vertex_position_loc, 
-                    3, gl.GL_FLOAT,False, stride, self.vertices 
+                gl.glVertexAttribPointer(
+                    self.Vertex_position_loc,
+                    3, gl.GL_FLOAT,False, stride, self.vertices
                 )
-                print "Bind 1"
 
                 gl.glVertexAttribPointer(
                 	self.Vertex_texCoord_loc,
                 	2, gl.GL_FLOAT,False, stride, self.vertices+12
                 )
-                
-                print "Bind 2"
+
                 gl.glVertexAttribPointer( 
-                    self.Vertex_normal_loc, 
+                    self.Vertex_normal_loc,
                     3, gl.GL_FLOAT,False, stride, self.vertices+20
                 )
-                print "Bind Done!"
-                
-                # musti diubah jadi indices biar lebih gampang
-                #gl.glDrawElements(gl.GL_TRIANGLES, 18, GL_UNSIGNED_INT, 0)
 
-                gl.glDrawArrays(gl.GL_TRIANGLES, 0, 18)
+                # musti diubah jadi indices biar lebih gampang
+                gl.glDrawElements(gl.GL_TRIANGLES, 18, gl.GL_UNSIGNED_INT, None)
+
+                #gl.glDrawArrays(gl.GL_TRIANGLES, 0, 18)
             finally:
                 self.vertices.unbind()
-                #self.indices.unbind()
-                
+                self.indices.unbind()
+
                 gl.glDisableVertexAttribArray( self.Vertex_position_loc )
                 gl.glDisableVertexAttribArray( self.Vertex_texCoord_loc )
                 gl.glDisableVertexAttribArray( self.Vertex_normal_loc )
@@ -228,4 +227,3 @@ class TestContext( BaseContext ):
 
 if __name__ == "__main__":
     TestContext.ContextMainLoop()
-
