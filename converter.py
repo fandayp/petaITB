8ea38246f5b8ae90343a6c9c265d456f73d5f7b0
hasnank <hasnurk@gmail.com>
2017-05-07 18:16:28 +0700

    Add hasna, without labtek X

--- converter.py
+++ converter.py
@@ -1,15 +1,17 @@
 # ((x - (WIDTH/2))/HEIGHT)*2 = x
 # ((z - (HEIGHT/2))/HEIGHT)*2 = z
+
+import sys
 WIDTH = 484
 HEIGHT = 568
 
-x_real = [87, 135, 135, 87] 
-z_real = [288, 288, 306, 306]
+x_real = [float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4])] 
+z_real = [float(sys.argv[5]), float(sys.argv[6]), float(sys.argv[7]), float(sys.argv[8])]
 
-h = 0.05
+h = float(sys.argv[9])
 
 x = [float(x - (WIDTH / 2)) / HEIGHT * 2 for x in x_real]
-z = [float(z - (WIDTH / 2)) / HEIGHT * 2 for z in z_real]
+z = [float(z - (HEIGHT / 2)) / HEIGHT * 2 for z in z_real]
 
 h = -1 + h
 for i in range(len(x)):
